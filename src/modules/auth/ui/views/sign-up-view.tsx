6e9dfd13a4a6c22f460013348cbfca4eeaa1ae49
Aditya <adityasainics10@gmail.com>
2025-05-31 19:32:35 +0530

    setup folder structure and layout

---
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@/components/ui/card"
+
+export const SignUpView = () => {
+  console.log("SignUpView rendered");
+  return (
+    <Card>
+      Sign Up View
+    </Card>
+  )
+}
